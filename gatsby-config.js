8190e20dfcb018af593bf97fd8da05c0fb95eac6
nirajgeorgian <niraj.kishore@stayology.com>
2020-10-12 03:01:08 +0530

    fixed css issue

--- gatsby-config.js
+++ gatsby-config.js
@@ -94,6 +94,7 @@ module.exports = {
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
     'gatsby-plugin-react-helmet',
-    'gatsby-plugin-typescript'
+    'gatsby-plugin-typescript',
+    'gatsby-plugin-styled-components'
   ]
 }

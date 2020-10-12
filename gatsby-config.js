f9d6fe69ea89d4e8916d8f4e28723ac9e24bdcb2
nirajgeorgian <niraj.kishore@stayology.com>
2020-10-12 09:44:00 +0530

    added google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -67,19 +67,7 @@ module.exports = {
         // The property ID; the tracking code won't be generated without it
         trackingId: 'UA-180316829-1',
         // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: false,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: ['/preview/**', '/do-not-track/me/too/'],
-        // Defers execution of google analytics script after page load
-        defer: false,
-        // Any additional optional fields
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
-        cookieDomain: 'example.com'
+        head: true
       }
     },
     {
